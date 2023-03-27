7017269c5d8534f1ce375752bb922cd373d74cfb
dusankg <dusan1998kg@gmail.com>
2023-03-27 23:42:58 +0200

    Bug fix - deleting coffee module

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoffeeMenuComponent } from './coffee-menu/coffee-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CoffeeComponent } from './coffee/coffee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoffeeMenuComponent,
     HomeComponent,
-    CoffeeComponent
   ],
   imports: [
     BrowserModule,
