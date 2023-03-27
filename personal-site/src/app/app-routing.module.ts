2c49b963d91a1221856fcf88efaf733c9963677e
dusankg <nemanjaranit@gmail.com>
2023-03-27 19:59:54 +0200

    Added coffee-menu

--- personal-site/src/app/app-routing.module.ts
+++ personal-site/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoffeeMenuComponent } from './coffee-menu/coffee-menu.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home-menu', component: CoffeeMenuComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoffeeMenuComponent } from './coffee-menu/coffee-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoffeeMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
