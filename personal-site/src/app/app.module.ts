a9cdff94d4144c70a019f2195620640df5f520e0
dusankg <dusan1998kg@gmail.com>
2023-03-27 23:41:25 +0200

    New menu design

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoffeeMenuComponent } from './coffee-menu/coffee-menu.component';
 import { HomeComponent } from './home/home.component';
+import { CoffeeComponent } from './coffee/coffee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoffeeMenuComponent,
-    HomeComponent
+    HomeComponent,
+    CoffeeComponent
   ],
   imports: [
     BrowserModule,
