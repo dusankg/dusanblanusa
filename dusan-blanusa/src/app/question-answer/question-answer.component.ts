d39e3e1b0a62a0591c29ff161802a3216b8e1172
dusankg <dusan1998kg@gmail.com>
2023-10-23 01:19:40 +0200

    Disable button while waiting

--- dusan-blanusa/src/app/question-answer/question-answer.component.ts
+++ dusan-blanusa/src/app/question-answer/question-answer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import { FactoryTarget } from '@angular/compiler';
 
 @Component({
   selector: 'app-question-answer',
@@ -12,20 +13,24 @@ import {Injectable} from '@angular/core';
 export class QuestionAnswerComponent {
   question: string = '';
   response: string = '';
-  
+  button_disabled: boolean = false;
 
   constructor(private http: HttpClient) {
     this.question = 'Who are you?';
     this.response = '';
+    this.button_disabled = false;
   }
 
   getAnswer() {
+    this.button_disabled = true;
     this.response = 'Thinking ...'
     const url = 'https://askdusan-fzvrxng5.b4a.run/ask';
     const params = new HttpParams().set('question', this.question);
 
     this.http.get(url, { params }).subscribe((data: any) => {
       this.response = data.answer;
+      this.button_disabled = false;
     });
+    
   }
 }
